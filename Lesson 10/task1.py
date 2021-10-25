498f8f7fb2e0fb9d2dc1977eeed7d86c264c7baf
Holodi <88102380+Holodi@users.noreply.github.com>
2021-10-25 22:23:21 +0300

    Create task1.py

---
+++ Lesson 10/task1.py
@@ -0,0 +1,13 @@
+class Person():
+
+    def __init__(self, firstname, lastname, age):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.age = age
+
+    def talk(self):
+        print(f"Hello, my name is {self.firstname} {self.lastname} and I'm {self.age} years old.\n")
+
+
+def task1(firstname, lastname, age):
+    Person(firstname, lastname, age).talk()
